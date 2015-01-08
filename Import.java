83e345c21ad0b1d8852415b85e115b3608db7e06
PigletKevin <sei_zkz2013@hotmail.com>
2015-01-08 19:49:22 +0800

    Update Import.java
    增加了“进价”一栏，可动态更新进价数据；

--- Import.java
+++ Import.java
@@ -13,38 +13,39 @@ import javax.swing.*;
  */
 public class Import extends JFrame // implements ActionListener
 {
-	JLabel imTypeAndPrice, sum;
+	JLabel imTypeAndPrice, sum,importPrice;
 	JButton increment, cancel;
 	JComboBox<String> typeBox;
-	Box hbox1, hbox2, hbox3;
-	JTextField numTextField = new JTextField(16);
-
+	Box hbox1, hbox2, hbox3,hbox4;
+	JTextField numTextField = new JTextField(8);
+	JTextField priceTextField = new JTextField(8);
+	
 	public static void main(String[] args) {
 		new Import().setVisible(true);
 	}
 
 	public Import()// constructor
 	{
-
 		this.setTitle("import");
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setBounds(100, 100, 300, 200);
+		this.setBounds(100,100, 250, 165);
 		this.setLocationByPlatform(true);
 		this.setResizable(false);//not allowed to change the size of the frame.
 		
 		typeBox = new JComboBox<>();
 		// add the types.
-		typeBox.addItem("ipad air——3100 RMB");
-		typeBox.addItem("ipad mini2——2500 RMB");
-		typeBox.addItem("iphone5c——2900 RMB");
-		typeBox.addItem("iphone5——3500 RMB");
-		typeBox.addItem("iphone5s——4300 RMB");
+		typeBox.addItem("Ipad MINI2");
+		typeBox.addItem("Ipad Air");
+		typeBox.addItem("Iphone 5C");
+		typeBox.addItem("Iphone 5");
+		typeBox.addItem("Iphone 5S");
 		typeBox.setEditable(false);
 		typeBox.setMaximumSize(typeBox.getPreferredSize());// ?
 
 		imTypeAndPrice = new JLabel("增加数量:");
 		sum = new JLabel("数量:");
-
+       importPrice=new JLabel("进价:");
+       
 		// set the type to the box
 		hbox1 = Box.createHorizontalBox();
 		hbox1.add(imTypeAndPrice);
@@ -68,10 +69,17 @@ public class Import extends JFrame // implements ActionListener
 		hbox3.add(cancel);
 		hbox3.add(Box.createHorizontalGlue());
 
+		priceTextField.setMaximumSize(priceTextField.getPreferredSize());
+		hbox4 = Box.createHorizontalBox();
+		hbox4.add(importPrice);
+		hbox4.add(Box.createHorizontalStrut(10));
+		hbox4.add(priceTextField);
+		
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		panel.add(hbox1);
 		panel.add(hbox2);
+		panel.add(hbox4);
 		panel.add(hbox3);
 
 		this.setContentPane(panel);
@@ -79,10 +87,12 @@ public class Import extends JFrame // implements ActionListener
 
 		// create listeners
 		increment.addActionListener(new ActionListener() {
-
-			WarehouseCheck updateCheck = new WarehouseCheck();// 实例化类，然后使用其中的数据变量
+			public void actionPerformed(ActionEvent e){}
+            //this part should be modified.
+			
+			/*WarehouseCheck updateCheck = new WarehouseCheck();// 实例化类，然后使用其中的数据变量
 			int info = Integer.parseInt(numTextField.getText());// 将字符转换为整型进行储存。
-
+             
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				if (typeBox.getName() == "ipad air——3100 RMB") {
@@ -99,21 +109,11 @@ public class Import extends JFrame // implements ActionListener
 				// new successImport().setVisible(true);
 				JOptionPane.showMessageDialog(null, "Import Correctly! ", "正确",
 						JOptionPane.YES_OPTION);
-			}
+			}*/
 		});
-		/*
-		 * //add the listener of the comboBox typeBox.addActionListener(new
-		 * ActionListener() {
-		 * 
-		 * public void actionPerformed(ActionEvent e) { //to add the operation }
-		 * });
-		 */
 		cancel.addActionListener(new ActionListener() {
-
-			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				System.exit(0);
+				//...
 			}
 		});
 	}
